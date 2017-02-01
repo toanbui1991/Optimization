77347994fbcb522079202003880284b5b97b5c5d
Toan <toanbui1991@gmail.com>
2017-02-01 15:04:27 +0700

    compute fib(n) with dynamic programming

---
+++ Optimization_KnapsackProblem_GreedyAlgorithm/KnapsackProblem_Optimization.py
@@ -0,0 +1,66 @@
+
+class Food(object):
+    def __init__(self, n, v, w):
+        self.name = n
+        self.value = v
+        self.calories = w
+    def getValue(self):
+        return self.value
+    def getCost(self):
+        return self.calories
+    def density(self):
+        return self.getValue()/self.getCost()
+    def __str__(self):
+        return self.name + ': <' + str(self.value)\
+                 + ', ' + str(self.calories) + '>'
+
+def buildMenu(names, values, calories):
+    """names, values, calories lists of same length.
+       name a list of strings
+       values and calories lists of numbers
+       returns list of Foods"""
+    menu = []
+    for i in range(len(values)):
+        menu.append(Food(names[i], values[i],
+                          calories[i]))
+    return menu
+
+def greedy(items, maxCost, keyFunction):
+    """Assumes items a list, maxCost >= 0,
+         keyFunction maps elements of items to numbers"""
+    itemsCopy = sorted(items, key = keyFunction,
+                       reverse = True)
+    result = []
+    totalValue, totalCost = 0.0, 0.0
+    for i in range(len(itemsCopy)):
+        if (totalCost+itemsCopy[i].getCost()) <= maxCost:
+            result.append(itemsCopy[i])
+            totalCost += itemsCopy[i].getCost()
+            totalValue += itemsCopy[i].getValue()
+    return (result, totalValue)
+
+def testGreedy(items, constraint, keyFunction):
+    taken, val = greedy(items, constraint, keyFunction)
+    print('Total value of items taken =', val)
+    for item in taken:
+        print('   ', item)
+
+def testGreedys(foods, maxUnits):
+    print('Use greedy by value to allocate', maxUnits,
+          'calories')
+    testGreedy(foods, maxUnits, Food.getValue)
+    print('\nUse greedy by cost to allocate', maxUnits,
+          'calories')
+    testGreedy(foods, maxUnits,
+               lambda x: 1/Food.getCost(x))
+    print('\nUse greedy by density to allocate', maxUnits,
+          'calories')
+    testGreedy(foods, maxUnits, Food.density)
+
+
+names = ['clock', 'picture', 'radio', 'vase', 'book', 'computer']
+values = [175,90,20,50,10,200]
+calories = [10,9,4,2,1,20]
+foods = buildMenu(names, values, calories)
+testGreedys(foods, 20)
+    
